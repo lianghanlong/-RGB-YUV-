0d15dedc6ca383e2c70ab42a19e0d4aeda4a3c58
pc-570 <admin@pc-570deiMac.local>
2017-12-11 12:25:51 +0800

    Initial Commit

---
+++ 视音频数据处理入门：RGB、YUV像素数据处理/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  视音频数据处理入门：RGB、YUV像素数据处理
+//
+//  Created by pc-570 on 2017/12/11.
+//  Copyright © 2017年 pc-570. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
